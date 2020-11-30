eef0196007b5ba9f3381b50f5f396bfe5cedee64
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-29 23:42:30 -0800

    Event listening

--- jQuery/working with Events/js/script.js
+++ jQuery/working with Events/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-	$('#example').on('click','.switch',function(){
-		console.log($(this));
-		$(this).parent().toggleClass('highlighted');
+	$('#example').on('click','button',function(){
+		var selected= $('#place option:selected');
+		var value= selected.val();
+		//var price=selected.attr('data-price')
+		var price= selected.data('price')
+		console.log(value);
+		console.log(price);
+		console.log(selected.attr('data-price'));
+		if(price){
+			$("#result").html(value+" "+price);
+		}
 	})
 })
