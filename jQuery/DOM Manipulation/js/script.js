1e0f02f75668444ceef985e11317b6ef24bccaf2
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-30 04:49:02 -0800

    empty()

--- jQuery/DOM Manipulation/js/script.js
+++ jQuery/DOM Manipulation/js/script.js
@@ -1,18 +1,9 @@
 $(document).ready(function(){
-        $('#add-container').on('click','button',function(){
-                var value = $('#add-container input').val();
-               
+       $('#container').html('<strong>Hello World</strong>');
 
-                var html =' <div class="item">\
-                <div class="remove">X</div>' + value + '</div>';
-                
-                $('#places-container').append(html);
-        });
-        //this doesn't work for new elements!
-       /* $('#places-container .remove').on('click',function(){
-        $(this).parent().remove();
-        })*/
-        $('#places-container').on('click','.remove',function(){
-                $(this).parent().remove();
-                })
-});
+       console.log($('#container').html());
+        
+       //$('#container').empty();
+       $('#container').attr('new-attribute',123);
+       console.log($('#container').attr('new-attribute'));
+})
