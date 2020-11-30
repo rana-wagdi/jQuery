85f451cce534e62858607965a61ca95ed7c316b9
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-29 23:54:15 -0800

    Change Events

--- jQuery/working with Events/js/script.js
+++ jQuery/working with Events/js/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
-	$('#example').on('click','button',function(){
-		var selected= $('#place option:selected');
-		var value= selected.val();
-		//var price=selected.attr('data-price')
-		var price= selected.data('price')
-		console.log(value);
-		console.log(price);
-		console.log(selected.attr('data-price'));
-		if(price){
-			$("#result").html(value+" "+price);
+	$('#example').on('change','select',function(){
+		var selected =$(this).find('option:selected');
+		var value =selected.val();
+		var price= selected.data('price');
+
+		if(price) {
+			$('#result').html(value+' '+price);
 		}
-	})
-})
+		
+	});
+});
