5ba29b34a0521bfe904aa79905aeaff95bb2b0ad
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-30 04:23:23 -0800

    append(),,prepend()

--- jQuery/DOM Manipulation/js/script.js
+++ jQuery/DOM Manipulation/js/script.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-	$('#add-container').on('click', 'button', function(){
-		var value = $('#add-container input').val();
-		console.log(value);
+$(document).ready(function(){
+        $('#add-container').on('click','button',function(){
+                var value = $('#add-container input').val();
+                console.log(value);
 
-		var html = '<div class="item">\
+                var html =' <div class="item">\
                 <div class="remove">X</div>' + value + '</div>';
-        console.log(html);
+                console.log(html);
 
-        //$('#places-container').append(html);
-        //$(html).appendTo('#places-container');
-
-        $('#places-container').prepend(html);
-        //$(html).prependTo('#places-container');
-	});
+              //  $('#places-container').append(html);
+             // $(html).appendTo('#places-container');
+                $('#places-container').prepend(html);
+        })
 });
