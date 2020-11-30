d635c1b25ec0725844bc54aa1a9ca30350e31d24
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-29 20:43:17 -0800

    Children Elements

--- jQuery/DOM Traversing/js/script.js
+++ jQuery/DOM Traversing/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	
 	//CSS selectors
-	var result = $('#animals .creature');
+	var result=$('#animals > .creature');
 	console.log(result);
 
-	//DOM traversing (more efficient)
-	result=$('#animals').find('.creature');
+	//DOM traversing
+	result=$('#animals').children('.creature');
 	console.log(result);
 });
