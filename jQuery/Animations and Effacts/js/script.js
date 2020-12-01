ab5fe5c9a11c1bd04325f07d5b606684ab3f89fa
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-30 20:31:18 -0800

    fade Effect

--- jQuery/Animations and Effacts/js/script.js
+++ jQuery/Animations and Effacts/js/script.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 	$('.item-box').on('click','a.info-link',function(e){
 		e.preventDefault();
 
-		//hide(), toggle()
-		//$(this).closest('.item-box').find('.more-info').show();
-		$(this).closest('.item-box').find('.more-info').toggle(1000);//slow,,fast,,1000(1s)
+		//fadeIn,  fadeOut(), fadeToggle()
+		$(this).closest('.item-box').find('.more-info').fadeToggle('slow');
 	});
 })
