43fee068f7f156cc9444dc6790f8f5cbdba0ed3e
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-30 20:24:47 -0800

    showing and hiding

--- jQuery/Animations and Effacts/js/script.js
+++ jQuery/Animations and Effacts/js/script.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
-	$('.item-box').on('click', 'a.info-link', function(event){
-		event.preventDefault();
+$(document).ready(function(){
+	$('.item-box').on('click','a.info-link',function(e){
+		e.preventDefault();
 
 		//hide(), toggle()
-		$(this).closest('.item-box').find('.more-info').toggle('slow', function(){
-			alert('finished');
-		});
+		//$(this).closest('.item-box').find('.more-info').show();
+		$(this).closest('.item-box').find('.more-info').toggle(1000);//slow,,fast,,1000(1s)
 	});
-});
+})
