e6bfc1ea8220339afe2bf622ed5e3a4fd2554bbe
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-30 20:49:08 -0800

    Animations for CSS properties

--- jQuery/Animations and Effacts/js/script.js
+++ jQuery/Animations and Effacts/js/script.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-	$('.item-box').on('click','a.info-link',function(e){
-		e.preventDefault();
-
-		//slideDown(), slideUp(), slideToggle()
-		$(this).closest('.item-box').find('.more-info').slideToggle('slow');
+	$('#example1').on('click',function(){
+		$(this).animate({
+			opacity:0.3
+		},500,function(){
+			alert('completed!');
+		});
 	});
-})
+		$('#example2').on('click',function(){
+			$(this).animate({
+				left:200,
+				width:40
+			},500);
+		});
+		$('#example3').on('click',function(){
+			$(this).animate({
+				left:'+=20',
+				width:'+=50'
+			},500);
+		});
+});
